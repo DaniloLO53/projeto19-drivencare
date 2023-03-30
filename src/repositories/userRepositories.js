b877a0fc4321b9b5ab655451b026c0bc53ccdb36
Danilo Leão <olin_ad@hotmail.com>
2023-03-30 11:26:59 -0300

    feat: userRepositories getByEmail and insertUser function

--- src/repositories/userRepositories.js
+++ src/repositories/userRepositories.js
@@ -1,9 +1,22 @@
+import db from "../database/database.js";
+
 async function getByEmail(email) {
-  return email;
+  return await db.query(
+    `    
+    SELECT * FROM users WHERE email=$1
+  `,
+    [email],
+  );
 }
 
-async function insertUser(data) {
-  return data;
+async function insertUser({ name, email, password, is_doctor, uuid }) {
+  return await db.query(
+    `
+        INSERT INTO users (name, email, password, is_doctor, uuid)
+        VALUES ($1, $2, $3, $4, $5)
+    `,
+    [name, email, password, is_doctor, uuid],
+  );
 }
 
 export default {
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,7 @@
 import userRepositories from "../repositories/userRepositories";
 import messages from "../utils/constants/messages";
 import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 
 async function create({ name, email, password, is_doctor }) {
   const { rows: users } = await userRepositories.getByEmail(email);
@@ -8,7 +9,15 @@ async function create({ name, email, password, is_doctor }) {
 
   if (user) {
     const hashPassword = await bcrypt.hash(password, 10);
-    await userRepositories.create({ name, email, password: hashPassword, is_doctor });
+    const uuid = uuidv4();
+
+    await userRepositories.insertUser({
+      name,
+      email,
+      password: hashPassword,
+      is_doctor,
+      uuid,
+    });
     return;
   }
 
