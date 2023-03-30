b818287b63c248740e38e976b531c869e4bc6409
Danilo Leão <olin_ad@hotmail.com>
2023-03-30 08:57:49 -0300

    feat: api config

--- src/config/app.js
+++ src/config/app.js
@@ -1,1 +1,9 @@
-// testing
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+export default app
---
+++ src/config/server.js
@@ -0,0 +1,11 @@
+import http from 'http'
+import dotenv from 'dotenv'
+import app from './app'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
