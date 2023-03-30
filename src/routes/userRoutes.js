9440b87d3515f546860bb599850a32415e76edf2
Danilo Leão <olin_ad@hotmail.com>
2023-03-30 11:48:04 -0300

    feat: users to signup and controller route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import userControllers from "../controllers/userControllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import signinSchema from "../schemas/signin.js";
 import signupSchema from "../schemas/signup.js";
 
 const userRoute = express.Router();
 
-userRoute.post("/sign-up", validateSchema(signupSchema));
+userRoute.post("/sign-up", validateSchema(signupSchema), userControllers.create);
 userRoute.post("/sign-in", validateSchema(signinSchema));
+
+export default userRoute;
