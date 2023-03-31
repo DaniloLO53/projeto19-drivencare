7b3431316d1b42d780ef15b4453191b95a553d07
Danilo Leão <olin_ad@hotmail.com>
2023-03-31 09:39:00 -0300

    feat: get route - search doctors by params

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userControllers from "../controllers/userControllers.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
+import paramSchema from "../schemas/params.js";
 import signinSchema from "../schemas/signin.js";
 import signupSchema from "../schemas/signup.js";
 
@@ -8,5 +9,10 @@ const userRoute = express.Router();
 
 userRoute.post("/sign-up", validateSchema(signupSchema), userControllers.create);
 userRoute.post("/sign-in", validateSchema(signinSchema), userControllers.enter);
+userRoute.get(
+  "/search/:name?/:specialization?/:district?/:city?/:state?",
+  validateSchema(paramSchema),
+  userControllers.get,
+);
 
 export default userRoute;
